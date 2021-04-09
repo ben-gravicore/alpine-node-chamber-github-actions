0984b1bb22ec6f2483089d7cce480a5aed530567
Benjamin <benjamin.chung@gravicore.io>
2021-04-09 16:37:20 -0700

    test chamber

--- src/react.sh
+++ src/react.sh
@@ -64,7 +64,7 @@ function reactUnitTests {
 }
 
 function reactPublishS3 {
-
+  echo "chamber exec $NAMESPACE-$DEPLOYMENT_STAGE -- env | grep -e $CHAMBER_S3_CDN_BUCKET_ID"
   chamberOutput=$(chamber exec $NAMESPACE-$DEPLOYMENT_STAGE -- env | grep -e $CHAMBER_S3_CDN_BUCKET_ID)
   chamberExitCode=${?}
   # Exit code of 0 indicates success. Print the output and exit.
