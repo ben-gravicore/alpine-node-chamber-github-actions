0bfebf7242f0afe6a3a824d2fbf1a5b6351983ce
Benjamin <benjamin.chung@gravicore.io>
2021-04-09 08:34:20 -0700

    add react unit tests

--- src/react.sh
+++ src/react.sh
@@ -37,3 +37,29 @@ function reactBuild {
 
   exit ${yarnExitCode}
 }
+
+
+function reactUnitTests {
+
+  chamberOutput=$(chamber export cel-borrower-$DEPLOYMENT_STAGE -f dotenv | grep -e AWS_REGION -e ADMIN_COGNITO_USER_POOL_ID -e ADMIN_COGNITO_CLIENT_ID -e ADMIN_COGNITO_IDENTITY_POOL_ID -e ADMIN_COGNITO_DOMAIN_NAME -e ADMIN_APPSYNC_URIS -e ADMIN_DATADOG_RUM_CLIENT_TOKEN -e ADMIN_DATADOG_RUM_APPLICATION_ID > .env)
+  chamberExitCode=${?}
+  # Exit code of 0 indicates success. Print the output and exit.
+  if [ ${chamberExitCode} -ne 0 ]; then
+    echo "react-build: error: chamber export failed"
+    echo "${chamberOutput}"
+    echo
+    exit ${chamberExitCode}
+  fi
+  
+  sed -i -e 's/^/REACT_APP_/' .env && cat .env
+  yarnOutput=$(echo CI=true yarn test --color)
+  yarnExitCode=${?}
+  
+  # Exit code of 0 indicates success. Print the output and exit.
+  if [ ${yarnExitCode} -ne 0 ]; then
+    echo "react-build: error: yarn test"
+    echo "${yarnOutput}"
+    echo
+    exit ${yarnExitCode}
+  fi
+}
